a16c8ce0c0f3b092e1edec0544c923f367f97cf7
SlushUwU <105487011+SlushUwU@users.noreply.github.com>
2023-01-10 21:48:06 +0100

    Add files via upload

---
+++ Blockweek_09.12.2023/DreieckRechner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DreieckRechner
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bitte gebe zwei Seitenlängen ein getrennt durch ein Kommer:");
+                string eingabe = Console.ReadLine();
+                string[] seiten = eingabe.Split(',');
+                double seite1 = Convert.ToDouble(seiten[0]);
+            
+                double seite2 = Convert.ToDouble(seiten[1]);
+                double seite3 = Math.Sqrt(Math.Pow(seite1, 2) + Math.Pow(seite2, 2));
+                // Berechne Umfang:
+            
+                double umfang = seite1 + seite2 + seite3;
+                // Berechne Fläche:
+            
+                double flaeche = seite1 * seite2 / 2;
+                // Ausgabe:
+            
+                Console.WriteLine("Die Seitenlängen sind: " + seite1 + ", " + seite2 + ", " + seite3);
+                Console.WriteLine("Der Umfang ist: " + umfang);
+            
+                Console.WriteLine("Die Fläche ist: " + flaeche);
+            }
+
+        }
+    }
+}
---
+++ Blockweek_09.12.2023/QuadratRechner/QuadratRechner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QuadratRechner
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bitte gebe die Seitenlänge des Quadrates ein:");
+                var seitenlaenge = Convert.ToDouble(Console.ReadLine());
+                var flaeche = seitenlaenge * seitenlaenge;
+                var umfang = seitenlaenge * 4;
+            
+                Console.WriteLine("Die Fläche des Quadrates beträgt: " + flaeche);
+                Console.WriteLine("Der Umfang des Quadrates beträgt: " + umfang);
+            }
+            
+        }
+    }
+}
