9b11ccfd2459c0be8026c00b40a6c64d32ca1951
voidlesity <voidlesity@gmail.com>
2023-02-15 13:44:04 +0100

    added c# folder for voidlesity

---
+++ Blockweek_13.02.2023/c#_voidlesity/Bruchrechner.cs
@@ -0,0 +1,170 @@
+﻿class Bruch
+{
+    private int zaehler;
+    private int nenner;
+
+    public Bruch(int zaehler, int nenner)
+    {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+        this.Kuerzen();
+    }
+
+    public int Zaehler
+    {
+        get { return zaehler; }
+    }
+
+    public int Nenner
+    {
+        get { return nenner; }
+    }
+
+    public static Bruch operator +(Bruch a, Bruch b)
+    {
+        int zaehler = a.zaehler * b.nenner + b.zaehler * a.nenner;
+        int nenner = a.nenner * b.nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public static Bruch operator -(Bruch a, Bruch b)
+    {
+        int zaehler = a.zaehler * b.nenner - b.zaehler * a.nenner;
+        int nenner = a.nenner * b.nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public static Bruch operator *(Bruch a, Bruch b)
+    {
+        int zaehler = a.zaehler * b.zaehler;
+        int nenner = a.nenner * b.nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public static Bruch operator /(Bruch a, Bruch b)
+    {
+        int zaehler = a.zaehler * b.nenner;
+        int nenner = a.nenner * b.zaehler;
+        return new Bruch(zaehler, nenner);
+    }
+
+    public static Bruch operator ^(Bruch a, int exponent)
+    {
+        int zaehler = (int)Math.Pow(a.zaehler, exponent);
+        int nenner = (int)Math.Pow(a.nenner, exponent);
+        return new Bruch(zaehler, nenner);
+    }
+
+    public static Bruch operator *(int skalar, Bruch a)
+    {
+        int zaehler = skalar * a.zaehler;
+        int nenner = a.nenner;
+        return new Bruch(zaehler, nenner);
+    }
+
+    private void Kuerzen()
+    {
+        int teiler = this.GgT(this.zaehler, this.nenner);
+        this.zaehler /= teiler;
+        this.nenner /= teiler;
+
+        if (this.nenner < 0)
+        {
+            this.zaehler *= -1;
+            this.nenner *= -1;
+        }
+    }
+
+    private int GgT(int a, int b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public override string ToString()
+    {
+        if (nenner == 1)
+        {
+            return zaehler.ToString();
+        }
+        else
+        {
+            return zaehler.ToString() + "/" + nenner.ToString();
+        }
+    }
+}
+
+class Bruchrechner
+{
+    static void Main(string[] args)
+    {
+        do {
+Console.WriteLine(@"
+Welche Rechenoperation möchtest du ausführen?:
+1. [+]
+2. [-]
+3. [*]
+4. [/]
+5. [^]
+6. [√]
+--------------------------------------");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Geben Sie den ersten Bruch ein (Zähler/Nenner):");
+        string[] bruch1String = Console.ReadLine().Split('/');
+        int zaehler1 = int.Parse(bruch1String[0]);
+        int nenner1 = int.Parse(bruch1String[1]);
+        Bruch bruch1 = new Bruch(zaehler1, nenner1);
+
+        Console.WriteLine("Geben Sie den zweiten Bruch ein (Zähler/Nenner):");
+        string[] bruch2String = Console.ReadLine().Split('/');
+        int zaehler2 = int.Parse(bruch2String[0]);
+        int nenner2 = int.Parse(bruch2String[1]);
+        Bruch bruch2 = new Bruch(zaehler2, nenner2);
+
+        Bruch ergebnis = new Bruch(0, 1);
+
+        switch (operation)
+        {
+            case "1":
+                ergebnis = bruch1 + bruch2;
+                break;
+            case "2":
+                ergebnis = bruch1 - bruch2;
+                break;
+            case "3":
+                ergebnis = bruch1 * bruch2;
+                break;
+            case "4":
+                ergebnis = bruch1 / bruch2;
+                break;
+            case "5":
+                Console.WriteLine("Geben Sie den Exponenten ein:");
+                int exponent = int.Parse(Console.ReadLine());
+                ergebnis = bruch1 ^ exponent;
+                break;
+            case "6":
+                ergebnis = new Bruch((int)Math.Sqrt(bruch1.Zaehler), (int)Math.Sqrt(bruch1.Nenner));
+                break;
+            default:
+                Console.WriteLine("Ungültige Eingabe!");
+                return;
+        }
+Console.Write(@"
+--------------------------------------
+
+Press Y to run the program again, or any other key to exit: ");
+        Console.WriteLine("Das Ergebnis ist: " + ergebnis);
+        } while (Console.ReadKey(true).Key == ConsoleKey.Y);
+        Console.Clear();
+    }
+}
---
+++ Blockweek_13.02.2023/c#_voidlesity/Zahlensystemumrechner.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+class NumberConverter
+{
+    private int _value;
+    private int _base;
+
+    public NumberConverter(int value, int numBase)
+    {
+        _value = value;
+        _base = numBase;
+    }
+
+    public int Value
+    {
+        get { return _value; }
+    }
+
+    public int Base
+    {
+        get { return _base; }
+    }
+
+    public static NumberConverter operator +(NumberConverter a, NumberConverter b)
+    {
+        if (a._base != b._base)
+        {
+            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
+        }
+
+        return new NumberConverter(a._value + b._value, a._base);
+    }
+
+    public static NumberConverter operator -(NumberConverter a, NumberConverter b)
+    {
+        if (a._base != b._base)
+        {
+            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
+        }
+
+        return new NumberConverter(a._value - b._value, a._base);
+    }
+
+    public static NumberConverter operator *(NumberConverter a, NumberConverter b)
+    {
+        if (a._base != b._base)
+        {
+            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
+        }
+
+        return new NumberConverter(a._value * b._value, a._base);
+    }
+
+    public static NumberConverter operator /(NumberConverter a, NumberConverter b)
+    {
+        if (a._base != b._base)
+        {
+            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
+        }
+
+        return new NumberConverter(a._value / b._value, a._base);
+    }
+
+    public NumberConverter ToBinary()
+    {
+        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 2), 2), 2);
+    }
+
+    public NumberConverter ToOctal()
+    {
+        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 8), 8), 8);
+    }
+
+    public NumberConverter ToDecimal()
+    {
+        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 10), 10), 10);
+    }
+
+    public NumberConverter ToHexadecimal()
+    {
+        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 16), 16), 16);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+Console.Write(@"
+  ____     _    _          ___         _             ___        _                 
+ |_  /__ _| |_ | |___ _ _ / __|_  _ __| |_ ___ _ __ | _ \___ __| |_  _ _  ___ _ _ 
+  / // _` | ' \| / -_) ' \\__ \ || (_-<  _/ -_) '  \|   / -_) _| ' \| ' \/ -_) '_|
+ /___\__,_|_||_|_\___|_||_|___/\_, /__/\__\___|_|_|_|_|_\___\__|_||_|_||_\___|_|  
+                               |__/                                               
+----------------------------------------------------------------------------
+");
+Console.ReadLine();
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the first number: ");
+            int value1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int value2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the base of the numbers (2, 8, 10, or 16): ");
+            int numBase = int.Parse(Console.ReadLine());
+
+            NumberConverter a = new NumberConverter(value1, numBase);
+            NumberConverter b = new NumberConverter(value2, numBase);
+
+Console.WriteLine(@"
+Welche Rechenoperation möchtest du ausführen?:
+1. [+]
+2. [-]
+3. [*]
+4. [/]
+--------------------------------------");
+            string operation = Console.ReadLine();
+
+            NumberConverter result = new NumberConverter(0, numBase);
+            switch (operation)
+            {
+                case "1":
+                    result = a + b;
+                    break;
+                case "2":
+                    result = a - b;
+                    break;
+                case "3":
+                    result = a * b;
+                    break;
+                case "4":
+                    result = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    break;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Result: " + result.Value);
+//ask for Restart
+Console.Write(@"
+--------------------------------------
+
+Press Y to run the program again, or any other key to exit: ");
+        } while (Console.ReadKey(true).Key == ConsoleKey.Y);
+        Console.Clear();
+    }
+}
