f6afe38e3074c69528d33426ecaafd76115a94df
Shiraiyano <94565274+Shiraiyano@users.noreply.github.com>
2023-02-15 14:14:44 +0100

    added shiraiyano's solutions

---
+++ Blockweek_13.02.2023/java_shiraiyano/Film.java
@@ -0,0 +1,199 @@
+class Schauspieler
+{
+    private String Vorname;
+    private String Nachname;
+    private String Spitzname;
+    private String Geburtsort;
+    private String Geburtsdatum;
+    private String Nationalitaet;
+    private double Groesse; //in Meter
+
+    public Schauspieler(String vorname, String nachname) {
+        Vorname = vorname;
+        Nachname = nachname;
+        Spitzname = "";
+        Geburtsort = "";
+        Geburtsdatum = "";
+        Nationalitaet = "";
+        Groesse = 0;
+    }
+
+    public Schauspieler(String vorname, String nachname, String spitzname, double groesse) {
+        Vorname = vorname;
+        Nachname = nachname;
+        Spitzname = spitzname;
+        Geburtsort = "";
+        Geburtsdatum = "";
+        Nationalitaet = "";
+        Groesse = groesse;
+    }
+
+    public Schauspieler(String vorname, String nachname, String spitzname, String geburtsort, String geburtsdatum, String nationalitaet, double groesse) {
+        Vorname = vorname;
+        Nachname = nachname;
+        Spitzname = spitzname;
+        Geburtsort = geburtsort;
+        Geburtsdatum = geburtsdatum;
+        Nationalitaet = nationalitaet;
+        Groesse = groesse;
+    }
+    
+    public Schauspieler() {
+        Vorname = "";
+        Nachname = "";
+        Spitzname = "";
+        Geburtsort = "";
+        Geburtsdatum = "";
+        Nationalitaet = "";
+        Groesse = 0;
+    }
+
+    public String get_Vorname() {
+        return Vorname;
+    }
+    public void set_Vorname(String vorname) {
+        Vorname = vorname;
+    }
+
+    public String get_Nachname() {
+        return Nachname;
+    }
+    public void set_Nachname(String nachname) {
+        Nachname = nachname;
+    }
+
+    public String get_Spitzname() {
+        return Spitzname;
+    }
+    public void set_Spitzname(String spitzname) {
+        Spitzname = spitzname;
+    }
+
+    public String get_Geburtsort() {
+        return Geburtsort;
+    }
+    public void set_Geburtsort(String geburtsort) {
+        Geburtsort = geburtsort;
+    }
+
+    public String get_Geburtsdatum() {
+        return Geburtsdatum;
+    }
+    public void set_Geburtsdatum(String geburtsdatum) {
+        Geburtsdatum = geburtsdatum;
+    }
+
+    public String get_Nationalitaet() {
+        return Nationalitaet;
+    }
+    public void set_Nationalitaet(String nationalitaet) {
+        Nationalitaet = nationalitaet;
+    }
+
+    public double get_Groesse() {
+        return Groesse;
+    }
+    public void set_Groesse(double groesse) {
+        Groesse = groesse;
+    }
+}
+
+public class Film
+{
+    private String Titel;
+    private int Erscheinungsjahr;
+    private String Genre;
+    private int Laufzeit; //in ganzen Minuten
+    private List<Schauspieler> Cast; //Schauspieler
+    private String Sprache;
+    private int FSK;
+
+    public Film(String titel, int erscheinungsjahr) {
+        Titel = titel;
+        Erscheinungsjahr = erscheinungsjahr;
+        Genre = "";
+        Laufzeit = 0;
+        Cast = new ArrayList<Schauspieler>();
+        Sprache = "";
+        FSK = 0;
+    }
+
+    public Film(String titel, int erscheinungsjahr, String genre, int laufzeit) {
+        Titel = titel;
+        Erscheinungsjahr = erscheinungsjahr;
+        Genre = genre;
+        Laufzeit = laufzeit;
+        Cast = new ArrayList<Schauspieler>();
+        Sprache = "";
+        FSK = 0;
+    }
+
+    public Film(String titel, int erscheinungsjahr, String genre, int laufzeit, List<Schauspieler> cast, String sprache, int fSK) {
+        Titel = titel;
+        Erscheinungsjahr = erscheinungsjahr;
+        Genre = genre;
+        Laufzeit = laufzeit;
+        Cast = cast;
+        Sprache = sprache;
+        FSK = fSK;
+    }
+
+    public Film() {
+        Titel = "";
+        Erscheinungsjahr = 0;
+        Genre = "";
+        Laufzeit = 0;
+        Cast = new ArrayList<Schauspieler>();
+        Sprache = "";
+        FSK = 0;
+    }
+    
+    public String get_Titel() {
+        return Titel;
+    }
+    public void set_Titel(String titel) {
+        Titel = titel;
+    }
+
+    public int get_Erscheinungsjahr() {
+        return Erscheinungsjahr;
+    }
+    public void set_Erscheinungsjahr(int erscheinungsjahr) {
+        Erscheinungsjahr = erscheinungsjahr;
+    }
+
+    public String get_Genre() {
+        return Genre;
+    }
+    public void set_Genre(String genre) {
+        Genre = genre;
+    }
+
+    public int get_Laufzeit() {
+        return Laufzeit;
+    }
+    public void set_Laufzeit(int laufzeit) {
+        Laufzeit = laufzeit;
+    }
+
+    public List<Schauspieler> get_Cast() {
+        return Cast;
+    }
+    public void set_Cast(List<Schauspieler> cast) {
+        Cast = cast;
+    }
+
+    public String get_Sprache() {
+        return Sprache;
+    }
+    public void set_Sprache(String sprache) {
+        Sprache = sprache;
+    }
+
+    public int get_FSK() {
+        return FSK;
+    }
+    public void set_FSK(int fSK) {
+        FSK = fSK;
+    }
+}
---
+++ Blockweek_13.02.2023/java_shiraiyano/Trupp.java
@@ -0,0 +1,162 @@
+class Trupp
+{
+    private String Name;
+    private int Level;
+    private int Entwicklungskosten;
+    private int Baukosten;
+    private boolean Elixier; // (0= dunkles Elixier und 1 = normales Elixier)
+    private int Trefferpunkte;
+    private double Schaden; // (Pro Schlag)
+    private int Bewegungsgeschwindigkeit;
+    private double Angriffsgeschwindigkeit;
+    private int Reichweite;
+    private int Wohnraum;
+    private boolean Einzel; // (Schadensart 0 = mehrere Ziele, 1 = einzelne Ziele)
+
+    public Trupp(String name, int level, int entwicklungskosten, int baukosten) {
+        Name = name;
+        Level = level;
+        Entwicklungskosten = entwicklungskosten;
+        Baukosten = baukosten;
+        Elixier = false;
+        Trefferpunkte = 0;
+        Schaden = 0;
+        Bewegungsgeschwindigkeit = 0;
+        Angriffsgeschwindigkeit = 0;
+        Reichweite = 0;
+        Wohnraum = 0;
+        Einzel = false;
+    }
+
+    public Trupp(String name, int level, int entwicklungskosten, int baukosten, boolean elixier, int reichweite) {
+        Name = name;
+        Level = level;
+        Entwicklungskosten = entwicklungskosten;
+        Baukosten = baukosten;
+        Elixier = elixier;
+        Trefferpunkte = 0;
+        Schaden = 0;
+        Bewegungsgeschwindigkeit = 0;
+        Angriffsgeschwindigkeit = 0;
+        Reichweite = reichweite;
+        Wohnraum = 0;
+        Einzel = false;
+    }
+
+    public Trupp(String name, int level, int entwicklungskosten, int baukosten, Boolean elixier, int trefferpunkte,
+            double schaden, int bewegungsgeschwindigkeit, double angriffsgeschwindigkeit, int reichweite, int wohnraum,
+            Boolean einzel) {
+        Name = name;
+        Level = level;
+        Entwicklungskosten = entwicklungskosten;
+        Baukosten = baukosten;
+        Elixier = elixier;
+        Trefferpunkte = trefferpunkte;
+        Schaden = schaden;
+        Bewegungsgeschwindigkeit = bewegungsgeschwindigkeit;
+        Angriffsgeschwindigkeit = angriffsgeschwindigkeit;
+        Reichweite = reichweite;
+        Wohnraum = wohnraum;
+        Einzel = einzel;
+    }
+
+    public Trupp() {
+        Name = "";
+        Level = 0;
+        Entwicklungskosten = 0;
+        Baukosten = 0;
+        Elixier = false;
+        Trefferpunkte = 0;
+        Schaden = 0;
+        Bewegungsgeschwindigkeit = 0;
+        Angriffsgeschwindigkeit = 0;
+        Reichweite = 0;
+        Wohnraum = 0;
+        Einzel = false;
+    }
+
+    public String get_Name() {
+        return Name;
+    }
+    public void set_Name(String name) {
+        Name = name;
+    }
+
+    public int get_Level() {
+        return Level;
+    }
+    public void set_Level(int level) {
+        Level = level;
+    }
+
+    public int get_Entwicklungskosten() {
+        return Entwicklungskosten;
+    }
+    public void set_Entwicklungskosten(int entwicklungskosten) {
+        Entwicklungskosten = entwicklungskosten;
+    }
+
+    public int get_Baukosten() {
+        return Baukosten;
+    }
+    public void set_Baukosten(int baukosten) {
+        Baukosten = baukosten;
+    }
+
+    public boolean get_Elixier() {
+        return Elixier;
+    }
+    public void set_Elixier(Boolean elixier) {
+        Elixier = elixier;
+    }
+
+    public int get_Trefferpunkte() {
+        return Trefferpunkte;
+    }
+    public void set_Trefferpunkte(int trefferpunkte) {
+        Trefferpunkte = trefferpunkte;
+    }
+
+    public double get_Schaden() {
+        return Schaden;
+    }
+    public void set_Schaden(double schaden) {
+        Schaden = schaden;
+    }
+
+    public int get_Bewegungsgeschwindigkeit() {
+        return Bewegungsgeschwindigkeit;
+    }
+    public void set_Bewegungsgeschwindigkeit(int bewegungsgeschwindigkeit) {
+        Bewegungsgeschwindigkeit = bewegungsgeschwindigkeit;
+    }
+
+    public double get_Angriffsgeschwindigkeit() {
+        return Angriffsgeschwindigkeit;
+    }
+    public void set_Angriffsgeschwindigkeit(double angriffsgeschwindigkeit) {
+        Angriffsgeschwindigkeit = angriffsgeschwindigkeit;
+    }
+
+    public int get_Reichweite() {
+        return Reichweite;
+    }
+    public void set_Reichweite(int reichweite) {
+        Reichweite = reichweite;
+    }
+
+    public int get_Wohnraum() {
+        return Wohnraum;
+    }
+    public void set_Wohnraum(int wohnraum) {
+        Wohnraum = wohnraum;
+    }
+
+    public boolean get_Einzel() {
+        return Einzel;
+    }
+    public void set_Einzel(Boolean einzel) {
+        Einzel = einzel;
+    }
+    
+}
