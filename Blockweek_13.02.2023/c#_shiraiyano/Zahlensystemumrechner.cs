4107cb2fe3fb4bac8dc4ad3ceef56040be72de65
Shiraiyano <94565274+Shiraiyano@users.noreply.github.com>
2023-02-16 09:29:44 +0100

    modified shiraiyano's Zahlensystemumrechner

--- Blockweek_13.02.2023/c#_shiraiyano/Zahlensystemumrechner.cs
+++ Blockweek_13.02.2023/c#_shiraiyano/Zahlensystemumrechner.cs
@@ -15,42 +15,42 @@ namespace Zahlensystemumrechner
             Converter Zahl2 = new Converter();
             Converter Ergebnis = new Converter();
 
-            string Zwischenergebnis = "";
+            string Endergebnis;
 
             Console.Write("Gib die Basis der ersten Zahl an (2, 8, 10, 16). \n");
             Zahl1.setAndCheckBasis();
             Zahl1.setAndCheckWert();
+            Zahl1.setDezimalform(Zahl1.BaseConvert(Zahl1.getWert(),Zahl1.getBasis()));
 
             Console.WriteLine("Gib die Basis der zweiten Zahl an (2, 8, 10, 16). \n");
             Zahl2.setAndCheckBasis();
             Zahl2.setAndCheckWert();
+            Zahl2.setDezimalform(Zahl2.BaseConvert(Zahl2.getWert(), Zahl2.getBasis()));
 
             Console.WriteLine("Welche Basis soll das Ergebnis haben?\n");
             Ergebnis.setAndCheckBasis();
             Console.WriteLine("Gib eine Rechenoperation an, die ausgeführt werden soll.\n");
             Ergebnis.setAndCheckOperation();
-
-            switch(Ergebnis.getOperation())
+            switch (Ergebnis.getOperation())
             {
                 case "+":
-                  
+                    Ergebnis.setDezimalform(Zahl1.getDezimalform() + Zahl2.getDezimalform());
                     break;
 
                 case "-":
-                   
+                    Ergebnis.setDezimalform(Zahl1.getDezimalform() - Zahl2.getDezimalform());
                     break;
 
                 case "*":
-                    
+                    Ergebnis.setDezimalform(Zahl1.getDezimalform() * Zahl2.getDezimalform());
                     break;
 
                 case "/":
-                    
+                    Ergebnis.setDezimalform(Zahl1.getDezimalform() / Zahl2.getDezimalform());
                     break;
-
             }
-
-            Console.WriteLine(Ergebnis.BaseConvert(Zwischenergebnis,Ergebnis.getBasis()));
+            Endergebnis = Convert.ToString(Ergebnis.getDezimalform(), Ergebnis.getBasis());
+            Console.WriteLine("Das Ergebnis ist: " + Endergebnis);
         }
     }
 
@@ -58,11 +58,12 @@ namespace Zahlensystemumrechner
     {
         private string Wert;
         private int Basis;
+        private int Dezimalform;
         private string Operation;
 
-        public string BaseConvert(string Wert, int Basis)
+        public int BaseConvert(string Wert, int Basis)
         {
-            string ConvWert = Convert.ToInt32(Wert, Basis).ToString();
+            int ConvWert = Convert.ToInt32(Wert, Basis);
             return ConvWert;
         }
 
@@ -89,12 +90,14 @@ namespace Zahlensystemumrechner
                 string NewWert = Console.ReadLine();
                 Console.WriteLine();
 
+                //Using LINQ to check if Number wants to use letters when its not a hex
                 if (getBasis() < 16 && NewWert.All(Char.IsDigit) != true)
                 {
                     Console.WriteLine("Nur Hexadezimalzahlen (Basis 16) dürfen Zahlen enthalten! \n");
                     Console.WriteLine("Drücke [Enter], um die Zahl erneut einzugeben!\n");
                     Console.ReadLine();
                 }
+                //Using LINQ to check for invalid Hexadecimal input (only 0 - 9, A - F)
                 else if (getBasis() == 16 && NewWert.All("0123456789abcdefABCDEF".Contains) != true)
                 {
                     Console.WriteLine("Hexadezimalzahlen dürfen nur Ziffern und Buchstaben von A bis F enthalten! \n");
@@ -106,8 +109,8 @@ namespace Zahlensystemumrechner
                     CorrectNumFormat = true;
                     Wert = NewWert;
                     Console.WriteLine();
-                }       
-            }while (CorrectNumFormat == false);
+                }
+            } while (CorrectNumFormat == false);
         }
 
         public int getBasis()
@@ -123,7 +126,7 @@ namespace Zahlensystemumrechner
 
                 if (NewBasis != 2 && NewBasis != 8 && NewBasis != 10 && NewBasis != 16)
                 {
-                    
+
                     Console.WriteLine("Die Basis muss 2, 8, 10 oder 16 sein!\n");
                     Console.WriteLine("Drücke [Enter], um die Basis erneut einzugeben!\n");
                     Console.ReadLine();
@@ -137,6 +140,10 @@ namespace Zahlensystemumrechner
             } while (CorrectBase == false);
         }
 
+        public int getDezimalform()
+        { return Dezimalform; }
+        public void setDezimalform(int dezimalform)
+        { Dezimalform = dezimalform;}
         public string getOperation()
         { return Operation; }
         public void setAndCheckOperation()
