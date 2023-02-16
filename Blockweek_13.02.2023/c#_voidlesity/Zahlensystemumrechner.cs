27280519df11c813408dd6ea22099d071331aa65
voidlesity <voidlesity@gmail.com>
2023-02-16 08:41:13 +0100

    updated Zahlensystem umrechner

--- Blockweek_13.02.2023/c#_voidlesity/Zahlensystemumrechner.cs
+++ Blockweek_13.02.2023/c#_voidlesity/Zahlensystemumrechner.cs
@@ -2,146 +2,162 @@
 
 class NumberConverter
 {
-    private int _value;
-    private int _base;
-
-    public NumberConverter(int value, int numBase)
+    private int decimalValue;
+    
+    public NumberConverter(int decimalValue)
     {
-        _value = value;
-        _base = numBase;
+        this.decimalValue = decimalValue;
     }
 
-    public int Value
+    public static NumberConverter FromBinary(string binaryValue)
     {
-        get { return _value; }
+        int decimalValue = Convert.ToInt32(binaryValue, 2);
+        return new NumberConverter(decimalValue);
     }
 
-    public int Base
+    public static NumberConverter FromOctal(string octalValue)
     {
-        get { return _base; }
+        int decimalValue = Convert.ToInt32(octalValue, 8);
+        return new NumberConverter(decimalValue);
+    }
+
+    public static NumberConverter FromDecimal(int decimalValue)
+    {
+        return new NumberConverter(decimalValue);
+    }
+
+    public static NumberConverter FromHexadecimal(string hexadecimalValue)
+    {
+        int decimalValue = Convert.ToInt32(hexadecimalValue, 16);
+        return new NumberConverter(decimalValue);
+    }
+
+    public string ToBinary()
+    {
+        return Convert.ToString(decimalValue, 2);
+    }
+
+    public string ToOctal()
+    {
+        return Convert.ToString(decimalValue, 8);
+    }
+
+    public int ToDecimal()
+    {
+        return decimalValue;
+    }
+
+    public string ToHexadecimal()
+    {
+        return Convert.ToString(decimalValue, 16);
     }
 
     public static NumberConverter operator +(NumberConverter a, NumberConverter b)
     {
-        if (a._base != b._base)
-        {
-            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
-        }
-
-        return new NumberConverter(a._value + b._value, a._base);
+        int sum = a.decimalValue + b.decimalValue;
+        return new NumberConverter(sum);
     }
 
     public static NumberConverter operator -(NumberConverter a, NumberConverter b)
     {
-        if (a._base != b._base)
-        {
-            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
-        }
-
-        return new NumberConverter(a._value - b._value, a._base);
+        int difference = a.decimalValue - b.decimalValue;
+        return new NumberConverter(difference);
     }
 
     public static NumberConverter operator *(NumberConverter a, NumberConverter b)
     {
-        if (a._base != b._base)
-        {
-            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
-        }
-
-        return new NumberConverter(a._value * b._value, a._base);
+        int product = a.decimalValue * b.decimalValue;
+        return new NumberConverter(product);
     }
 
     public static NumberConverter operator /(NumberConverter a, NumberConverter b)
     {
-        if (a._base != b._base)
-        {
-            throw new Exception("Numbers must be in the same base to perform arithmetic operations.");
-        }
-
-        return new NumberConverter(a._value / b._value, a._base);
-    }
-
-    public NumberConverter ToBinary()
-    {
-        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 2), 2), 2);
-    }
-
-    public NumberConverter ToOctal()
-    {
-        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 8), 8), 8);
-    }
-
-    public NumberConverter ToDecimal()
-    {
-        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 10), 10), 10);
-    }
-
-    public NumberConverter ToHexadecimal()
-    {
-        return new NumberConverter(Convert.ToInt32(Convert.ToString(_value, 16), 16), 16);
+        int quotient = a.decimalValue / b.decimalValue;
+        return new NumberConverter(quotient);
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        Console.Clear();
 Console.Write(@"
   ____     _    _          ___         _             ___        _                 
  |_  /__ _| |_ | |___ _ _ / __|_  _ __| |_ ___ _ __ | _ \___ __| |_  _ _  ___ _ _ 
   / // _` | ' \| / -_) ' \\__ \ || (_-<  _/ -_) '  \|   / -_) _| ' \| ' \/ -_) '_|
  /___\__,_|_||_|_\___|_||_|___/\_, /__/\__\___|_|_|_|_|_\___\__|_||_|_||_\___|_|  
                                |__/                                               
-----------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 ");
-Console.ReadLine();
-        do
-        {
+        Console.ReadLine();
+        do {
             Console.Clear();
-            Console.WriteLine("Enter the first number: ");
-            int value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geben Sie die Basis der Zahlen ein (2, 8, 10, 16):");
+            int baseValue = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the second number: ");
-            int value2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geben Sie die erste Zahl ein:");
+            string value1 = Console.ReadLine();
 
-            Console.WriteLine("Enter the base of the numbers (2, 8, 10, or 16): ");
-            int numBase = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geben Sie die zweite Zahl ein:");
+            string value2 = Console.ReadLine();
 
-            NumberConverter a = new NumberConverter(value1, numBase);
-            NumberConverter b = new NumberConverter(value2, numBase);
+            NumberConverter number1, number2;
 
-Console.WriteLine(@"
-Welche Rechenoperation möchtest du ausführen?:
-1. [+]
-2. [-]
-3. [*]
-4. [/]
---------------------------------------");
-            string operation = Console.ReadLine();
-
-            NumberConverter result = new NumberConverter(0, numBase);
-            switch (operation)
+            switch (baseValue)
             {
-                case "1":
-                    result = a + b;
+                case 2:
+                    number1 = NumberConverter.FromBinary(value1);
+                    number2 = NumberConverter.FromBinary(value2);
                     break;
-                case "2":
-                    result = a - b;
+                case 8:
+                    number1 = NumberConverter.FromOctal(value1);
+                    number2 = NumberConverter.FromOctal(value2);
                     break;
-                case "3":
-                    result = a * b;
+                case 10:
+                    number1 = NumberConverter.FromDecimal(Convert.ToInt32(value1));
+                    number2 = NumberConverter.FromDecimal(Convert.ToInt32(value2));
                     break;
-                case "4":
-                    result = a / b;
+                case 16:
+                    number1 = NumberConverter.FromHexadecimal(value1);
+                    number2 = NumberConverter.FromHexadecimal(value2);
                     break;
                 default:
-                    Console.WriteLine("Invalid operation.");
+                    Console.WriteLine("Ungültige Basis.");
+                    return;
+            }
+
+            Console.WriteLine("Welche Rechenoperation möchten Sie durchführen (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            NumberConverter result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = number1 + number2;
                     break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine("Ungültige Rechenoperation.");
+                    return;
             }
 
             Console.Clear();
-            Console.WriteLine("Result: " + result.Value);
-//ask for Restart
+            Console.WriteLine("Ergebnis: " + result.ToDecimal());
+            Console.WriteLine("Ergebnis in Binär: " + result.ToBinary());
+            Console.WriteLine("Ergebnis in Oktal: " + result.ToOctal());
+            Console.WriteLine("Ergebnis in Hexadezimal: " + result.ToHexadecimal());
+
+        //ask for Restart
 Console.Write(@"
 --------------------------------------
 
