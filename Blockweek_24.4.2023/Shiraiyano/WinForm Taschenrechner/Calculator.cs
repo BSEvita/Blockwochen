5361d206be161cfcbca904f119df0187a3c0ade4
Shiraiyano <said.riedl@gmail.com>
2023-04-25 10:23:49 +0200

    Added Calculatro Logic, added Sin Cos Tan Graphical Output

---
+++ Blockweek_24.4.2023/Shiraiyano/WinForm Taschenrechner/Calculator.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForm_Taschenrechner
+{
+	public class Calculations
+	{
+		public string Operation;
+
+		public double FirstNumber { get; set; }
+		public double SecondNumber { get; set; }
+		public double Result { get; set; }
+
+		public static double Addition(double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber + SecondNumber;
+		}
+
+		public static double Subtraction(double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber - SecondNumber;
+		}
+
+		public static double Multiplikation(double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber * SecondNumber;
+		}
+
+		public static double Division(double FirstNumber, double SecondNumber)
+		{
+			return FirstNumber / SecondNumber;
+		}
+
+		public static double Potenzieren(double FirstNumber, double SecondNumber)
+		{
+			return Math.Pow(FirstNumber, SecondNumber);
+		}
+
+		public static double WurzelZiehen(double FirstNumber, double SecondNumber)
+		{
+			return Math.Pow(FirstNumber, 1.0 / SecondNumber);
+		}
+
+		public static double Logarithmus(double FirstNumber, double SecondNumber)
+		{
+			return Math.Log(FirstNumber, SecondNumber);
+		}
+
+		public static double Factorial(double FirstNumber)
+		{
+			double Factorial = FirstNumber;
+			for (double i = Factorial - 1; i > 0; i--)
+			{
+				FirstNumber *= i;
+			}
+			return FirstNumber;
+		}
+
+		public static double Sinus(double FirstNumber)
+		{
+			return Math.Sin(FirstNumber * Math.PI / 180);
+		}
+
+		public static double Cosinus(double FirstNumber)
+		{
+			return Math.Cos(FirstNumber * Math.PI / 180);
+		}
+
+		public static double Tangens(double FirstNumber)
+		{
+			return Math.Tan(FirstNumber * Math.PI / 180);
+		}
+
+		public void Equate(string Operation, double number1, double number2)
+		{
+
+			FirstNumber = number1;
+			SecondNumber = number2;
+
+			switch (Operation)
+			{
+				case "+":
+					Result = Addition(FirstNumber, SecondNumber);
+					break;
+				case "-":
+					Result = Subtraction(FirstNumber, SecondNumber);
+					break;
+				case "*":
+					Result = Multiplikation(FirstNumber, SecondNumber);
+					break;
+				case "/":
+					Result = Division(FirstNumber, SecondNumber);
+					break;
+				case "x^y":
+					Result = Potenzieren(FirstNumber, SecondNumber);
+					break;
+				case "(x)√y":
+					Result = WurzelZiehen(FirstNumber, SecondNumber);
+					break;
+				case "log_x(y)":
+					Result = Logarithmus(FirstNumber, SecondNumber);
+					break;
+				case "!":
+					Result = Factorial(FirstNumber);
+					break;
+				case "Sin":
+					Result = Sinus(FirstNumber);
+					break;
+				case "Cos":
+					Result = Cosinus(FirstNumber);
+					break;
+				case "Tan":
+					Result = Tangens(FirstNumber);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
--- Blockweek_24.4.2023/Shiraiyano/WinForm Taschenrechner/Taschenrechner.cs
+++ Blockweek_24.4.2023/Shiraiyano/WinForm Taschenrechner/Taschenrechner.cs
@@ -2,105 +2,144 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WinForm_Taschenrechner
 {
-    public partial class Taschenrechner : Form
-    {
-        //Boolean Flags, um zu Überprüfen, ob zuletzt ein Operator oder eine Zahl gedrückt wurde.
-        bool LastClickedIsNumber = true;
-        bool LastClickedKomma = false;
-        bool LastClickedIsOperator;
+	public partial class Taschenrechner : Form
+	{
+		//Objekt der Rechner-Logik Klasse
+		private Calculations calculations = new Calculations();
 
-        double Eingabe;
+		string NumberString;
 
-        double Zwischensumme; //selbsterklärend
-        double Endsumme;      //selbsterklärend
 
-        int PrecedencePrevOp;
-        int PrecedenceCurOp;
 
-        //Neuer Operator ersetzt bei Eingabe den vorherigen. Alter Operator wird zum Entfernen in Anzeige benötigt.
-        string LastOperator;
+		//Boolean Flags, um zu Überprüfen, ob zuletzt ein Operator oder eine Zahl gedrückt wurde.
+		bool LastClickedIsNumber = true;
+		bool LastClickedKomma = false;
+		bool LastClickedIsTransf = false;
+		bool LastClickedIsOperator;
 
-        public Taschenrechner()
-        {
-            InitializeComponent();
-        }
 
-        private void OperatorIstGleich_Click(object sender, EventArgs e)
-        {
+		//Neuer Operator ersetzt bei Eingabe den vorherigen. Alter Operator wird zum Entfernen in Anzeige benötigt.
+		string LastOperator;
 
-        }
+		public Taschenrechner()
+		{
+			InitializeComponent();
+		}
 
-        private void AuthorsNote_Click(object sender, EventArgs e)
-        {
+		private void OperatorIstGleich_Click(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void Number_Click(object sender, EventArgs e)
-        {
-            Button Number = (Button)sender;
+		private void AuthorsNote_Click(object sender, EventArgs e)
+		{
 
-            if (LastClickedIsNumber || OutputField.Text.Length == 0)
-            {
-                OutputField.Text += Number.Text;
-            }
-            else if (LastClickedIsOperator && OutputField.Text.Length > 0)
-            {
-                OutputField.Text = OutputField.Text.Remove(0, OutputField.Text.Length);
-                OutputField.Text += Number.Text;
-            }
+		}
 
-            LastClickedIsNumber = true;
-            LastClickedIsOperator = false;
-        }
-        private void SymbolKomma_Click(object sender, EventArgs e)
-        {
-            Button Komma = (Button)sender;
-            if (LastClickedIsNumber && OutputField.Text.Length > 0 && LastClickedKomma == false)
-            {
-                OutputField.Text += Komma.Text;
-                LastClickedKomma = true;
-            }
-        }
+		private void Number_Click(object sender, EventArgs e)
+		{
+			Button Number = (Button)sender;
 
-        private void Operator_Click(object sender, EventArgs e)
-        {
-            Button Operator = (Button)sender;
+			OutputField.Text += Number.Text;
 
-            if (LastClickedIsNumber && OutputField.Text.Length > 0)
-            {
-                AllPreviouslyEntered.Text += " " + OutputField.Text + " " + Operator.Text;
-            }
-            else if (LastClickedIsOperator && AllPreviouslyEntered.Text.Length > 0)
-            {
-                AllPreviouslyEntered.Text = AllPreviouslyEntered.Text.Substring(0, AllPreviouslyEntered.Text.Length - LastOperator.Length) + Operator.Text;
-            }
+			NumberString += Number.Text;
 
-            LastOperator = Operator.Text;
-            LastClickedIsNumber = false;
-            LastClickedIsOperator = true;
-            LastClickedKomma = false;
-        }
+			LastClickedIsNumber = true;
+			LastClickedIsOperator = false;
+			LastClickedIsTransf = false;
+		}
 
-        private void EingabeRückgängig_Click(object sender, EventArgs e)
-        {
-            switch (OutputField.Text.Length)
-            {
-                case 0:
-                    break;
-                default:
-                    OutputField.Text = OutputField.Text.Remove(OutputField.Text.Length - 1, 1);
-                    break;
-            }
-        }
+		private void SymbolKomma_Click(object sender, EventArgs e)
+		{
+			Button Komma = (Button)sender;
+			if (LastClickedIsNumber && OutputField.Text.Length > 0 && LastClickedKomma == false)
+			{
+				OutputField.Text += Komma.Text;
+				LastClickedKomma = true;
+			}
+		}
 
-        private void LöschenCE_Click(object sender, EventArgs e)
-        {
-            if (OutputField.Text.Length == 0)
-            {
-                AllPreviouslyEntered.Text = AllPreviouslyEntered.Text.Remove(0, AllPreviouslyEntered.Text.Length);
-            }
-            OutputField.Text = OutputField.Text.Remove(0, OutputField.Text.Length);
+		private void Operator_Click(object sender, EventArgs e)
+		{
+			Button Operator = (Button)sender;
 
-        }
-    }
+			if (LastClickedIsNumber && OutputField.Text.Length > 0)
+			{
+				AllPreviouslyEntered.Text += " " + OutputField.Text + " " + Operator.Text;
+			}
+			else if (LastClickedIsOperator && AllPreviouslyEntered.Text.Length > 0)
+			{
+				AllPreviouslyEntered.Text = AllPreviouslyEntered.Text.Substring(0, AllPreviouslyEntered.Text.Length - LastOperator.Length) + Operator.Text;
+			}
+			else if (LastClickedIsTransf)
+				AllPreviouslyEntered.Text += " " + Operator.Text;
+
+			OutputField.Text = OutputField.Text.Remove(0, OutputField.Text.Length);
+
+			NumberString = "";
+
+			LastOperator = Operator.Text;
+
+			LastClickedIsNumber = false;
+			LastClickedIsOperator = true;
+			LastClickedKomma = false;
+		}
+
+		private void EingabeRückgängig_Click(object sender, EventArgs e)
+		{
+			switch (OutputField.Text.Length)
+			{
+				case 0:
+					break;
+				default:
+					OutputField.Text = OutputField.Text.Remove(OutputField.Text.Length - 1, 1);
+					break;
+			}
+		}
+
+		private void LöschenCE_Click(object sender, EventArgs e)
+		{
+			if (OutputField.Text.Length == 0)
+			{
+				AllPreviouslyEntered.Text = AllPreviouslyEntered.Text.Remove(0, AllPreviouslyEntered.Text.Length);
+			}
+			OutputField.Text = OutputField.Text.Remove(0, OutputField.Text.Length);
+
+		}
+
+		private void OutputField_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void NumberTransformationClick(object sender, EventArgs e)
+		{
+			Button NumberTransform = (Button)sender;
+
+			if (LastClickedIsNumber && OutputField.Text.Length > 0)
+			{
+				switch (NumberTransform.Text)
+				{
+					case "Sin":
+						AllPreviouslyEntered.Text += " Sin(" + NumberString + ")";
+						break;
+					case "Cos":
+						AllPreviouslyEntered.Text += " Cos(" + NumberString + ")";
+						break;
+					case "Tan":
+						AllPreviouslyEntered.Text += " Tan(" + NumberString + ")";
+						break;
+					case "x^y":
+						OutputField.Text += " ^ ";
+						break;
+				}
+
+				OutputField.Text = OutputField.Text.Remove(0, OutputField.Text.Length);
+
+				LastClickedIsTransf = true;
+				LastClickedIsNumber = false;
+				LastClickedIsOperator = false;
+			}
+		}
+	}
 }
