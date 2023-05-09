fe04ee79e01739f8502e19f5140eeea830c64abf
Alexander <66317031+ByAlexius@users.noreply.github.com>
2023-05-09 10:20:21 +0200

    Update Form1.cs
    Signed-off-by: Alexander <66317031+ByAlexius@users.noreply.github.com>

--- Blockweek_24.4.2023/Slush/Bruchrechner/Form1.cs
+++ Blockweek_24.4.2023/Slush/Bruchrechner/Form1.cs
@@ -3,6 +3,11 @@ namespace Bruchrechner
     public partial class Form1 : Form
     {
         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             double bruchTopFirstDouble = Convert.ToDouble(bruchTopFirst.Text);
