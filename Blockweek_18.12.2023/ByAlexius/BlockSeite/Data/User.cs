ff819bdf39ea417cbb4825cdec93dde51b59db47
ByAlexius <Alexander.Rziha@hotmail.com>
2023-12-19 14:35:18 +0100

    Added my things

---
+++ Blockweek_18.12.2023/ByAlexius/BlockSeite/Data/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlockSeite.Data
+{
+    [Table(name: "Users")]
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(name: "userid")]
+        public int UserId { get; set; }
+
+        [Column(name: "username")]
+        public string UserName { get; set; }
+
+        [Column(name: "email")]
+        public string email { get; set; }
+    }
+}
