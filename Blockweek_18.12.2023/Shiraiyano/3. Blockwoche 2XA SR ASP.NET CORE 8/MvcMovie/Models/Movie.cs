9d6695e53e25da0c397afd94c22805f2b1382871
Shiraiyano <said.riedl@gmail.com>
2023-12-19 15:57:12 +0100

    Finalized the Tutorial Project, will maybe change stuff later, but whatever.

--- Blockweek_18.12.2023/Shiraiyano/3. Blockwoche 2XA SR ASP.NET CORE 8/MvcMovie/Models/Movie.cs
+++ Blockweek_18.12.2023/Shiraiyano/3. Blockwoche 2XA SR ASP.NET CORE 8/MvcMovie/Models/Movie.cs
@@ -7,15 +7,25 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+
+    [StringLength(60, MinimumLength = 3), Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+
+    [Display(Name = "Release Date"), Required, DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
     public string? Genre { get; set; }
 
+
+    [Range(1, 100), Required, DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), Required, StringLength(5)]
     public string? Rating { get; set; }
 }
