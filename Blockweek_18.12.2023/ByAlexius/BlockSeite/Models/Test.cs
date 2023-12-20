536fcb6571c0aa8318b3334ba061061558413faa
ByAlexius <Alexander.Rziha@hotmail.com>
2023-12-20 15:07:13 +0100

    some more stuff

--- Blockweek_18.12.2023/ByAlexius/BlockSeite/Models/Test.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlockSeite.Models
-{
-    public class Test
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Role> Roles { get; set; }
-    }
-}
