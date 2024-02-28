87b9c4c02c19cb3b7852a35b3943f22eadd23fce
Haon607 <noah0676@gmail.com>
2024-02-28 14:06:00 +0100

    hamsta changes

---
+++ Blockweek_26.2.2024/Hamsta/Aufgabe_4/TutorialWebAPI2/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialWebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private static Dictionary<string, string> userCredentials = new Dictionary<string, string>();
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] UserCredentials credentials)
+        {
+            if (userCredentials.ContainsKey(credentials.Username))
+            {
+                return Conflict("Username already exists");
+            }
+
+            userCredentials.Add(credentials.Username, credentials.Password);
+
+            return Ok("User registered successfully");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserCredentials credentials)
+        {
+            if (!userCredentials.ContainsKey(credentials.Username))
+            {
+                return NotFound("User not found");
+            }
+
+            string storedPassword = userCredentials[credentials.Username];
+
+            if (credentials.Password != storedPassword)
+            {
+                return Unauthorized("Invalid password");
+            }
+
+            return Ok("Login successful");
+        }
+    }
+
+    public class UserCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
