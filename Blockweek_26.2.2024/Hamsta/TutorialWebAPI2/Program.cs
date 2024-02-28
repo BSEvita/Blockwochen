233a427dc0c40030b0a48fbf126ecdf411e425e2
Haon607 <noah0676@gmail.com>
2024-02-28 09:42:33 +0100

    Hamsta changes

---
+++ Blockweek_26.2.2024/Hamsta/TutorialWebAPI2/Program.cs
@@ -0,0 +1,45 @@
+using BookStore.Data.Database;
+using BookStore.Data.Interface;
+using BookStore.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookContext>(options =>
+{
+    options.UseInMemoryDatabase("BooksDatabase");
+});
+
+builder.Services.AddScoped<DatabaseInitializerService>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbInitializer = services.GetRequiredService<DatabaseInitializerService>();
+    dbInitializer.Initialize();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
