255211978297cf67b22d4c1a9fe6ca4999e38dac
akama-aka <105487011+akama-aka@users.noreply.github.com>
2024-03-07 11:05:04 +0100

    Added Task 3. feat. @byAlexius
    Signed-off-by: akama-aka <105487011+akama-aka@users.noreply.github.com>

---
+++ Blockweek_26.2.2024/AkamaAka/Task_3/CRUDAPI/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using CRUDAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CRUDAPI.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
