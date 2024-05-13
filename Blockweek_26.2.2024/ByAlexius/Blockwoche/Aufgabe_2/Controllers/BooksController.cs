2c8bb8347fb9c277e8858548b7346b9831af46d6
ByAlexius <Alexander.Rziha@hotmail.com>
2024-05-13 15:23:44 +0200

    Fixed some more things

--- Blockweek_26.2.2024/ByAlexius/Blockwoche/Aufgabe_2/Controllers/BooksController.cs
+++ Blockweek_26.2.2024/ByAlexius/Blockwoche/Aufgabe_2/Controllers/BooksController.cs
@@ -30,4 +30,22 @@ public class BooksController : ControllerBase
             return NotFound();
         return book;
     }
+
+    [HttpPost()]
+    public ActionResult<Book> CreateBook([FromBody]Book book)
+    {
+        _bookRepository.CreateBook(book);
+        return book;
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteBook(int id)
+    {
+        var book = _bookRepository.GetBook(id);
+        if (book == null)
+            return NotFound();
+        
+        _bookRepository.Delete(id);
+        return Accepted();
+    }
 }
